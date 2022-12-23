b727beb2cd0618a9f7c238b46a88f4c69a96f7af
mandimbaj <mandimbaj@who.int>
2022-12-23 15:07:53 +0100

    insert models

--- app/Models/DureeLocation.php
+++ app/Models/DureeLocation.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class DureeLocation extends Model
 {
     use HasFactory;
+
 }
--- app/Models/Tarification.php
+++ app/Models/Tarification.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Tarification extends Model
 {
     use HasFactory;
+
+    public function dureeLocation()
+    {
+        return $this->belongsTo(DureeLocation::class, "duree_location_id", "id");
+    }
+
+    public function article()
+    {
+        return $this->belongsTo(Article::class);
+    }
 }

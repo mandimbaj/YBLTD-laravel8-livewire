d26ffd8b09d2d8bba8698c045b2d31c29d25e4ce
mandimbaj <mandimbaj@who.int>
2025-01-10 19:45:44 +0100

    create database and tables

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,7 +8,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Starter</title>
+    <title>YBLTD</title>
     <link rel="stylesheet" href="{{mix("css/app.css")}}">
     @livewireStyles
     <script data-cfasync="false" nonce="c1f2cd01-7d24-42d4-978b-798e2617d509">
